3fbda75529f579c3f87312359d13c90d8fc8ea2d
gardheamsantos00 <gardheam.santos@yahoo.com.br>
2017-05-20 23:15:49 -0300

    api sem front-end

---
+++ src/main/java/br/com/ufc/apipessoa/ApipessoaApplication.java
@@ -0,0 +1,12 @@
+package br.com.ufc.apipessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApipessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApipessoaApplication.class, args);
+	}
+}
